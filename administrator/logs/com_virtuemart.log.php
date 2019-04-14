a7c01375749139ce84e67d79c8acea80c1cb3790
NETV2000 <vazquezeduardo611@gmail.com>
2019-04-14 14:38:12 -0600

    version6nestor

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,13 @@
+#
+#<?php die("Forbidden."); ?>
+
+2019-04-14 20:08:33 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:08:39 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:08:43 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:08:47 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:08:56 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:09:40 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:09:47 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:10:02 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:31:37 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
+2019-04-14 20:31:41 ERROR vmError: La ruta para facturas parece que necesita los permisos correctos Ruta segura C:\xampp\htdocs\Joomla\administrator\components\com_virtuemart\3ZSpzs6op93MzBMvaPhIa\
